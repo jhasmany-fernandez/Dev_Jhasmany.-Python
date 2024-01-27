c796b2393c5557961e33d24aeb968d71aa1f7d1c
jhasmany jhunnior fernandez ortega <jhasmany.fernandez.dev@gmail.com>
2024-01-27 10:36:24 -0400

    numeros enteros, 20 %

--- Ejercicios/Numeros Enteros/ClassNEnteros.py
+++ Ejercicios/Numeros Enteros/ClassNEnteros.py
@@ -1,19 +1,24 @@
 class NumeroEntero:
+    # Constructor
     def __init__(self, valor):
         self.valor = valor
 
+    # Suma entera
     def sumar(self, otro_numero):
         resultado = self.valor + otro_numero.valor
         return NumeroEntero(resultado)
 
+    # Resta entera
     def restar(self, otro_numero):
         resultado = self.valor - otro_numero.valor
         return NumeroEntero(resultado)
 
+    # Multiplicación entera
     def multiplicar(self, otro_numero):
         resultado = self.valor * otro_numero.valor
         return NumeroEntero(resultado)
 
+    # División entera
     def dividir(self, otro_numero):
         if otro_numero.valor == 0:
             print("No es posible dividir por cero.")
@@ -21,12 +26,15 @@ class NumeroEntero:
         resultado = self.valor / otro_numero.valor
         return NumeroEntero(int(resultado))
     
+    # Contar la cantidad de dígitos
     def contarDigitos(self):
         return len(str(self.valor))
     
+    # Invertir los dígitos
     def invertirDigito(self, valor):
         return int(str(valor)[::-1])
     
+    # Mostrar la cantidad de dígitos pares
     def mostrarCantidadPares(self):
         cantidad = 0
         for i in str(self.valor):
@@ -34,4 +42,55 @@ class NumeroEntero:
                 cantidad += 1
         return cantidad
     
+    # Mostrar la cantidad de dígitos impares
+    def mostrarCantidadImpares(self):
+        cantidad = 0
+        for i in str(self.valor):
+            if int(i) % 2 != 0:
+                cantidad += 1
+        return cantidad
+    
+    # Mostrar los números pares
+    def mostrarNumerosPares(self):
+        lista = []
+        for i in str(self.valor):
+            if int(i) % 2 == 0:
+                lista.append(i)
+        return lista
+    
+    # Mostrar los números impares
+    def mostrarNumerosImpares(self):
+        lista = []
+        for i in str(self.valor):
+            if int(i) % 2 != 0:
+                lista.append(i)
+        return lista
 
+    # Mostrar los números primos
+    def mostrarNumerosPrimos(self):
+        lista = []
+        for i in str(self.valor):
+            if int(i) % 2 != 0:
+                lista.append(i)
+        return lista
+    
+    
+# Solicitar al usuario que ingrese valores por consola
+valor1 = int(input("Ingrese el valor para num1: "))
+valor2 = int(input("Ingrese el valor para num2: "))
+
+# Crear instancias de la clase NumeroEntero con los valores ingresados por el usuario
+num1 = NumeroEntero(valor1)
+num2 = NumeroEntero(valor2)
+
+# Ejemplo de uso
+suma = num1.sumar(num2)
+#resta = num1.restar(num2)
+#multiplicacion = num1.multiplicar(num2)
+#division = num1.dividir(num2)
+
+# Mostrar resultados
+print("Suma:", suma.valor)
+#print("Resta:", resta.valor)
+#print("Multiplicación:", multiplicacion.valor)
+#print("División:", division.valor)    
--- Ejercicios/Numeros Enteros/main.py
+++ Ejercicios/Numeros Enteros/main.py
@@ -9,12 +9,16 @@ def mostrar_menu():
     print("5. Contar Dígitos")
     print("6. Invertir Dígitos")
     print("7. Cantidad de Dígitos Pares")
+    print("8. Cantidad de Dígitos Impares")
+    print("9. Mostrar Números Pares")
+    print("10. Mostrar Números Impares")
+    print("11. Mostrar Números Primos")
     print("0. Salir")
 
 if __name__ == '__main__':
     while True:
         mostrar_menu()
-        opcion = input("Seleccione una opción (0-10): ")
+        opcion = input("Seleccione una opción (0-15): ")
 
         if opcion == "0":
             print("Saliendo del programa. ¡Hasta luego!")
@@ -23,30 +27,30 @@ if __name__ == '__main__':
             valor1 = int(input("Ingrese el valor para num1: "))
             num1 = NumeroEntero(valor1)
             valor2 = int(input("Ingrese el valor para num2: "))
-            num1 = NumeroEntero(valor1)
+            num2 = NumeroEntero(valor2)
             suma = num1.sumar(num2)
-            print("Suma:", suma.valor)
+            print("Suma es:", suma.valor)
         elif opcion == "2":
             valor1 = int(input("Ingrese el valor para num1: "))
             num1 = NumeroEntero(valor1)
             valor2 = int(input("Ingrese el valor para num2: "))
             num1 = NumeroEntero(valor1)
             resta = num1.restar(num2)
-            print("Resta:", resta.valor)
+            print("Resta es:", resta.valor)
         elif opcion == "3":
             valor1 = int(input("Ingrese el valor para num1: "))
             num1 = NumeroEntero(valor1)
             valor2 = int(input("Ingrese el valor para num2: "))
             num1 = NumeroEntero(valor1)
             multiplicacion = num1.multiplicar(num2)
-            print("Multiplicación:", multiplicacion.valor)
+            print("Multiplicación es:", multiplicacion.valor)
         elif opcion == "4":
             valor1 = int(input("Ingrese el valor para num1: "))
             num1 = NumeroEntero(valor1)
             valor2 = int(input("Ingrese el valor para num2: "))
             num1 = NumeroEntero(valor1)
             division = num1.dividir(num2)
-            print("División:", division.valor)
+            print("División es:", division.valor)
         elif opcion == "5":
             valor1 = int(input("Ingrese el valor: "))
             num1 = NumeroEntero(valor1)
@@ -59,5 +63,21 @@ if __name__ == '__main__':
             valor1 = int(input("Ingrese el valor: "))
             num1 = NumeroEntero(valor1)
             print("Cantidad de dígitos pares:", num1.mostrarCantidadPares())
+        elif opcion == "8":
+            valor1 = int(input("Ingrese el valor: "))
+            num1 = NumeroEntero(valor1)
+            print("Cantidad de dígitos impares:", num1.mostrarCantidadImpares())
+        elif opcion == "9":
+            valor1 = int(input("Ingrese el valor: "))
+            num1 = NumeroEntero(valor1)
+            print("Números pares:", num1.mostrarNumerosPares())
+        elif opcion == "10":
+            valor1 = int(input("Ingrese el valor: "))
+            num1 = NumeroEntero(valor1)
+            print("Números impares:", num1.mostrarNumerosImpares())
+        elif opcion == "11":        
+            valor1 = int(input("Ingrese el valor: "))
+            num1 = NumeroEntero(valor1)
+            print("Números primos:", num1.mostrarNumerosPrimos())        
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.")
